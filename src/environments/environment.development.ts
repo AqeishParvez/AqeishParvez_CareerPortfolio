174ccb8843fd0a9260c1c95e6ef168fc491e935c
AqeishParvez <aqeish.parvez@gmail.com>
2023-04-22 19:13:32 -0400

    env changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //apiUrl: "https://aqeishparvez-comp229.azurewebsites.net/api"
-    apiUrl: "http://localhost:3000/api"
+    apiUrl: "https://aqeishparvez-comp229.azurewebsites.net/api"
+    //apiUrl: "http://localhost:3000/api"
 };
