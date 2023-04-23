bff3e3dbcecfc62c397cb43b5a6581e3280dfe74
AqeishParvez <aqeish.parvez@gmail.com>
2023-04-22 20:52:30 -0400

    minor changes

--- src/app/content/contact/contact.component.ts
+++ src/app/content/contact/contact.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+sendButtonClick() {
+  alert("Your message was succesfully sent")
+}
 
 }
